dfb8ce8a0e298a3e066d490d4699455796f301e7
John Doe <johndoe@example.com>
2023-10-06 14:07:29 +0200

    Premier jet : selection par première lettre

--- serveur.py
+++ serveur.py
@@ -2,24 +2,41 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 import pymongo
 from pymongo import MongoClient
 import json
+from time import time
 
 
-def connect_mongo_db():
-    # Connexion à la base de données. User: mon_user, pswd: mon_mot_de_passe
+def connect_mongo_db(lettre:str):
+    print("Connecting to MongoDB...................")
     client = MongoClient('mongodb://mon_user:mon_mot_de_passe@localhost:27017/')
-    # Récupération de la base de données
     db = client['spotify']
     collection = db['spotifyCollection']
-    print("Connexion à la base de données réussie !")
+    # trackname must start with the letter lettre.
+    query = {'trackName': {'$regex': '^' + lettre}}
+    # get only the values of trackName and artistName
+    projection = {'trackName': 1, 'artistName': 1, '_id': 0}
+    # get the result of the query
+    result = collection.find(query, projection)
+    return result
 
 
 app = Flask(__name__)
 
-@app.route('/home', methods=['GET'])
-def home():
+@app.route('/', methods=['GET'])
+def index():
     return render_template('index.html')
 
 
+@app.route('/home', methods=['GET'])
+def home():
+    query = request.args.get('query')
+    print("Query1: ", query)
+    if query is None:
+        query = ""
+    print("Query2: ", query)
+    result = connect_mongo_db(query)
+    documents = [document for document in result]
+    return render_template('index.html', result=documents)
+
+
 if __name__ == '__main__':
-    connect_mongo_db()
     app.run(host='0.0.0.0')
