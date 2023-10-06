6542b7ca82bd856836611d26bb8229327126301b
John Doe <johndoe@example.com>
2023-10-06 14:48:36 +0200

    Maj structure code

--- serveur.py
+++ serveur.py
@@ -2,24 +2,38 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 import pymongo
 from pymongo import MongoClient
 import json
-
-
-def connect_mongo_db():
-    # Connexion à la base de données. User: mon_user, pswd: mon_mot_de_passe
-    client = MongoClient('mongodb://mon_user:mon_mot_de_passe@localhost:27017/')
-    # Récupération de la base de données
-    db = client['spotify']
-    collection = db['spotifyCollection']
-    print("Connexion à la base de données réussie !")
+from time import time
 
 
 app = Flask(__name__)
 
-@app.route('/home', methods=['GET'])
-def home():
+def connect_mongo_db():
+    print("Connecting to MongoDB...................")
+    client = MongoClient('mongodb://mon_user:mon_mot_de_passe@localhost:27017/')
+    db = client['spotify']
+    collection = db['spotifyCollection']
+    return collection
+
+
+
+@app.route('/get-result-from-lettre', methods=['GET'])
+def get_result_from_lettre():
+    lettre = request.args.get('lettre')
+    query = {'trackName': {'$regex': '^' + lettre}}
+    projection = {'trackName': 1, 'artistName': 1, '_id': 0}
+    result = collection.find(query, projection)
+    data = [r for r in result]
+    return render_template('index.html', result=data)
+
+
+
+
+
+@app.route('/', methods=['GET'])
+def index():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    connect_mongo_db()
+    collection = connect_mongo_db()
     app.run(host='0.0.0.0')
