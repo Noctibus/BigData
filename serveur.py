184a8718852fa57cf33a970fcb84ef361271c503
John Doe <johndoe@example.com>
2023-10-06 14:47:12 +0200

    Meilleures methodes de declaration des fonctions

--- serveur.py
+++ serveur.py
@@ -5,38 +5,35 @@ import json
 from time import time
 
 
-def connect_mongo_db(lettre:str):
+app = Flask(__name__)
+
+def connect_mongo_db():
     print("Connecting to MongoDB...................")
     client = MongoClient('mongodb://mon_user:mon_mot_de_passe@localhost:27017/')
     db = client['spotify']
     collection = db['spotifyCollection']
-    # trackname must start with the letter lettre.
+    return collection
+
+
+
+@app.route('/get-result-from-lettre', methods=['GET'])
+def get_result_from_lettre():
+    lettre = request.args.get('lettre')
     query = {'trackName': {'$regex': '^' + lettre}}
-    # get only the values of trackName and artistName
     projection = {'trackName': 1, 'artistName': 1, '_id': 0}
-    # get the result of the query
     result = collection.find(query, projection)
-    return result
+    data = [r for r in result]
+    return render_template('index.html', result=data)
 
 
-app = Flask(__name__)
+
+
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 
-@app.route('/home', methods=['GET'])
-def home():
-    query = request.args.get('query')
-    print("Query1: ", query)
-    if query is None:
-        query = ""
-    print("Query2: ", query)
-    result = connect_mongo_db(query)
-    documents = [document for document in result]
-    return render_template('index.html', result=documents)
-
-
 if __name__ == '__main__':
+    collection = connect_mongo_db()
     app.run(host='0.0.0.0')
