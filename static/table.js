71098c33fce5a18c35838585dfaefcb095cb92cf
FannyGobbo <fanny.gobbo00@gmail.com>
2023-10-13 10:31:55 +0200

    KAN-3 affichage réduit du tableau avec navigation

---
+++ static/table.js
@@ -0,0 +1,23 @@
+const itemsPerPage = 25;
+let page = 0;
+const table = document.getElementsByClassName("dataframe");
+
+function show_page (index) {
+    const rows = table[0].tBodies[0].rows;
+    if (index < 0) {
+        show_page(0);
+    } else if (index > rows.length){
+        show_page(index - 25);
+    } else {
+        page = index;
+    }
+      
+    for (let i = 0; i < rows.length; i++) {
+        if (i >= page && i < page + itemsPerPage) {
+          rows[i].style.display = 'table-row';
+        }
+        else {
+          rows[i].style.display = 'none';    
+        }
+    }
+}
